5cb976204002a7609ccd77f2b9aa9ea08805d518
Ammar Yasser Allaithy <ammaryasserallaithy@gmail.com>
2024-10-07 01:43:17 +0300

    add SpeedDuration component

---
+++ src/components/SpeedDuration.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { humanize } from "@/api/utils.js";
+
+const props = defineProps({
+  speed: String,
+  seconds: Number,
+  label: String,
+});
+
+const clacDuration = () => humanize(props.seconds / parseFloat(props.speed));
+</script>
+
+<template>
+  <p>
+    <span v-if="label" class="dim">{{ label }}:</span>
+    <span v-else class="dim">{{ speed }}x:</span>
+    {{ clacDuration() }}
+  </p>
+</template>
