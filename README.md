4bd1fd14e39c65eb5e5bdfeafa4d5ff67fbe35c5
Ammar Yasser Allaithy <ammaryasserallaithy@gmail.com>
2024-10-07 01:04:48 +0300

    just created

---
+++ README.md
@@ -0,0 +1,35 @@
+# YTPyzer
+
+**YouTube playlist analyzer**
+
+Uses YouTube Data API v3 to retrieve and analyze most common data of YouTube playlists and videos as listed
+
+**Playlist**
+
+- Name
+- Description
+- Date
+  - Published
+  - Last modified
+- Channel
+- Total duration (with different speeds)
+- Videos (count)
+
+**Video**
+
+- Image
+- Name
+- Description
+- Duration
+- Channel
+- Date
+  - Published
+  - Added to playlist
+- Statistics
+  - Views count
+  - Likes count
+  - Comments count
+- Language
+  - Caption (if exists)
+- Live status
+- Quality Definition
---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import Header from "./components/Header.vue";
+import Home from "./views/HomeView.vue";
+import About from "./views/AboutView.vue";
+
+import { ref } from "vue";
+
+const viewHome = ref(true);
+</script>
+
+<template>
+  <Header @nav="(isHome) => (viewHome = isHome)" />
+
+  <h1 class="center">
+    <span class="clr">Y</span>ou<span class="clr">T</span>ube
+    <span class="clr">P</span>laylist Anal<span class="clr">yzer</span>
+  </h1>
+
+  <Home v-show="viewHome" />
+  <About v-show="!viewHome" />
+</template>
+
+<style scoped>
+h1 {
+  margin: 2rem;
+}
+</style>
